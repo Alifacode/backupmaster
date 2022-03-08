c07d7dc10295e2e5dc72f7648222aa397e5050cc
Alifa <99187764+Alifacode@users.noreply.github.com>
2022-03-08 21:40:51 +0700

    Update alifacode-update-daun-berguguran.js

--- alifacode-update-daun-berguguran.js
+++ alifacode-update-daun-berguguran.js
@@ -5,7 +5,7 @@
 			******************************************/
 			  
 			  //Configure below to change URL path to the snow image
-			  var snowsrc="https://imgur.com/a/IjzqvCU";
+			  var snowsrc="https://i.postimg.cc/kVCVG7L0/ALIFA-DAUN.png";
 			  // Configure below to change number of snow to render
 			  var no = 20;
 			  // Configure whether snow should disappear after x seconds (0=never):
@@ -40,7 +40,7 @@
 			  am = new Array();
 			  stx = new Array();
 			  sty = new Array();
-			  snowsrc=(snowsrc.indexOf("dynamicdrive.com")!=-1)? "IjzqvCU" : snowsrc
+			  snowsrc=(snowsrc.indexOf("dynamicdrive.com")!=-1)? "ALIFA-DAUN.png" : snowsrc
 			  for (i = 0; i < no; ++ i) {  
 				dx[i] = 0;                        // set coordinate variables
 				xp[i] = Math.random()*(doc_width-50);  // set position variables
